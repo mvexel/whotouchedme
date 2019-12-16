9df4ec749cad4b3f0dbad2ca0c3fdda991e642e9
Martijn van Exel <m@rtijn.org>
2019-12-16 12:04:09 -0700

    adding --shallow switch

--- README.md
+++ README.md
@@ -27,8 +27,9 @@ And four optional arguments:
 * `--skipnodes` --> Don't look at nodes
 * `--skipways` --> Don't look at ways
 * `--skiprelations` --> Don't look at relations
-* `--keylist KEYLIST` --> A comma-separated list of OSM tag keys. Only features that have any of these keys will be considered.
+* `--keylist KEYLIST` --> A comma-separated list of OSM tag keys. Only features that have any of these keys will be considered
 * `--includemine` --> Include `osm_username`'s edits in output
+* `--shallow` --> Only include modifications made after `osm_username`'s **last** edit
 
 ## Output
 
@@ -43,6 +44,4 @@ The tool will output 3 CSV files with the following columns:
 
 There will be one file for each type of OSM feature: `nodes.csv`, `ways.csv`, `relations.csv`, unless you skip one of the feature types, see above.
 
-Each file will have one line for an OSM feature version that superseded a version edited by you. if you toggle `--includemine`, your own edits will also be included.
-
-The output will contain all newer edits since the *first* edit you made to a feature.
+Each file will have one line for an OSM feature version that superseded a version edited by you. if you toggle `--includemine`, your own edits will also be included. The output will contain all newer edits since the *first* edit you made to a feature, unless you use `--shallow`.
--- whoedited.py
+++ whoedited.py
@@ -10,60 +10,81 @@ import osmium
 
 
 class WhoEditedHandler(osmium.SimpleHandler):
-    def __init__(self, osm_username, keys, nodes=True, ways=True, relations=True, includemine=True):
+    def __init__(self, osm_username, keys, nodes=True, ways=True, relations=True, includemine=True, shallow=True):
         osmium.SimpleHandler.__init__(self)
         self.osm_username = osm_username
         self.keys = keys
         self.process_nodes = nodes
         if nodes:
-            self.my_edited_nodes = set()
-            self.other_edited_nodes = set()
+            self.mynodes_ids = set()
+            self.nodes_out = {}
         self.process_ways = ways
         if ways:
-            self.my_edited_ways = set()
-            self.other_edited_ways = set()
+            self.myways_ids = set()
+            self.ways_out = {}
         self.process_relations = relations
         if relations:
-            self.my_edited_relations = set()
-            self.other_edited_relations = set()
+            self.myrelations_ids = set()
+            self.relations_out = {}
         self.includemine = includemine
+        self.shallow = shallow
 
     @staticmethod
-    def make_summary(o):
-        return (o.id, o.version, o.changeset, o.timestamp, o.user, o.deleted)
+    def make_summary(o, out_dict):
+        record = (o.version, o.changeset, o.timestamp, o.user, o.deleted)
+        if o.id in out_dict:
+            out_dict[o.id].append(record)
+        else:
+            out_dict[o.id] = [record]
 
     def node(self, n):
         if self.process_nodes and (
             any(item in self.keys for item in [tag.k for tag in n.tags])
                 or not self.keys):
             if n.user == self.osm_username:
-                self.my_edited_nodes.add(n.id)
+                self.mynodes_ids.add(n.id)
+                if self.shallow:
+                    self.relations_out[n.id] = []
                 if self.includemine:
-                    self.other_edited_nodes.add(self.make_summary(n))
-            elif n.id in self.my_edited_nodes:
-                self.other_edited_nodes.add(self.make_summary(n))
+                    self.make_summary(n, self.nodes_out)
+            elif n.id in self.mynodes_ids:
+                self.make_summary(n, self.nodes_out)
 
     def way(self, w):
         if self.process_ways and (
             any(item in self.keys for item in [tag.k for tag in w.tags])
                 or not self.keys):
             if w.user == self.osm_username:
-                self.my_edited_ways.add(w.id)
+                self.myways_ids.add(w.id)
+                if self.shallow:
+                    self.relations_out[w.id] = []
                 if self.includemine:
-                    self.other_edited_ways.add(self.make_summary(w))
-            elif w.id in self.my_edited_ways:
-                self.other_edited_ways.add(self.make_summary(w))
+                    self.make_summary(w, self.ways_out)
+            elif w.id in self.myways_ids:
+                self.make_summary(w, self.ways_out)
 
     def relation(self, r):
         if self.process_relations and (
             any(item in self.keys for item in [tag.k for tag in r.tags])
                 or not self.keys):
             if r.user == self.osm_username:
-                self.my_edited_relations.add(r.id)
+                self.myrelations_ids.add(r.id)
+                if self.shallow:
+                    self.relations_out[r.id] = []
                 if self.includemine:
-                    self.other_edited_relations.add(self.make_summary(r))
-            elif r.id in self.my_edited_relations:
-                self.other_edited_relations.add(self.make_summary(r))
+                    self.make_summary(r, self.relations_out)
+            elif r.id in self.myrelations_ids:
+                self.make_summary(r, self.relations_out)
+
+
+def flatten(outdict):
+    result = []
+    for key in outdict:
+        for item in outdict[key]:
+            record = [key]
+            record.extend(item)
+            result.append(record)
+    return result
 
 
 def main():
@@ -81,6 +102,8 @@ def main():
                         help='Do not output relations')
     parser.add_argument('--includemine', action='store_true',
                         help='Do not output relations')
+    parser.add_argument('--shallow', action='store_true',
+                        help='Only consider my last edit to any feature')
 
     args = parser.parse_args()
 
@@ -94,24 +117,28 @@ def main():
         not args.skipnodes,
         not args.skipways,
         not args.skiprelations,
-        args.includemine)
+        args.includemine,
+        args.shallow)
     osmium_handler.apply_file(args.pbf_history_file)
 
     # write out result files
     if not args.skipnodes:
+        flat_nodeslist = flatten(osmium_handler.nodes_out)
         with open(os.path.abspath(os.path.join(args.out_path, 'nodes.csv')), 'w') as fh:
             csvwriter = csv.writer(fh)
-            for row in sorted(osmium_handler.other_edited_nodes, key=itemgetter(0, 1)):
+            for row in sorted(flat_nodeslist, key=itemgetter(0, 1)):
                 csvwriter.writerow(row)
     if not args.skipways:
-        with open(os.path.abspath(os.path.join(args.out_path, 'ways.csv')), 'w') as fh:
+        flat_wayslist = flatten(osmium_handler.ways_out)
+        with open(os.path.abspath(os.path.join(args.out_path.items(), 'ways.csv')), 'w') as fh:
             csvwriter = csv.writer(fh)
-            for row in sorted(osmium_handler.other_edited_ways, key=itemgetter(0, 1)):
+            for row in sorted(flat_wayslist, key=itemgetter(0, 1)):
                 csvwriter.writerow(row)
     if not args.skiprelations:
+        flat_relationslist = flatten(osmium_handler.relations_out)
         with open(os.path.abspath(os.path.join(args.out_path, 'relations.csv')), 'w') as fh:
             csvwriter = csv.writer(fh)
-            for row in sorted(osmium_handler.other_edited_relations, key=itemgetter(0, 1)):
+            for row in sorted(flat_relationslist, key=itemgetter(0, 1)):
                 csvwriter.writerow(row)
 
 
